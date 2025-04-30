d00d61d9a4ca586b7a0c474162fa20ec97966bf5
daisylee101 <daisyl43@nycstudents.net>
2025-04-30 16:31:01 +0000

    donezo

--- Sign.java
+++ Sign.java
@@ -1,23 +1,27 @@
 public class Sign {
     private String message;
     private int width;
-
-    public Sign(String message, int width) {
-        this.message = message;
-        this.width = width;
+    public Sign(String m, int w) {
+        message = m;
+        width = w;
     }
-}
-public int numberOfLines() {
-    // return message.length() / width + 1;
-}
 
-public String getLines() {
-    String s = "";
-    int i = 0;
-    while(i < message.length()){
-        s += message.substring(i, i +1); 
-        i++;
-        if(i % width == 0 ) s += ";";
+    public int numberOfLines() {
+        int lines = message.length() / width;
+        if(message.length() % width == 0) return lines;
+        return lines + 1;
+        //return message.length() / width + 1;
     }
-    return s;
+
+    public String getLines() {
+        String s = "";
+        int i = 0;
+        if(message.length() == 0) return null;
+        while(i < message.length()){
+            s += message.substring(i, i +1); 
+            i++;
+            if(i % width == 0 && i != message.length()) s += ";";
+        }
+        return s;
+    }
 }
