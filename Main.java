d3c8b5abd6552c060febc0c394b880e27fad8bc5
daisylee101 <daisyl43@nycstudents.net>
2025-04-30 14:59:41 +0000

    page 2/3?

--- Main.java
+++ Main.java
@@ -2,3 +2,10 @@ public class Main {
     public static void main(String[] args) {
         String str;
         int x;
+    
+        Sign sign1 = new Sign("ABC222DE", 3);
+        x = sign1.numberOfLines();
+        System.out.println(x);
+        // for now - str = sign1.getLines();
+        // for now - System.out.println(str);
+
--- Sign.java
+++ Sign.java
@@ -1,1 +1,23 @@
-helloooooo
+public class Sign {
+    private String message;
+    private int width;
+
+    public Sign(String message, int width) {
+        this.message = message;
+        this.width = width;
+    }
+}
+public int numberOfLines() {
+    // return message.length() / width + 1;
+}
+
+public String getLines() {
+    String s = "";
+    int i = 0;
+    while(i < message.length()){
+        s += message.substring(i, i +1); 
+        i++;
+        if(i % width == 0 ) s += ";";
+    }
+    return s;
+}
